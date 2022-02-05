8e09d7dfe4a564223fbdef62fb878b0973b894f8
alex <lolmagexe@gmail.com>
2022-02-05 12:24:50 +0200

    initial commit

---
+++ src/atoms/HeaderButton/index.jsx
@@ -0,0 +1,19 @@
+import './style.scss'
+
+import {Component} from 'react'
+
+class HeaderButton extends Component {
+    constructor (props) {
+        super(props)
+
+
+    }
+
+    render() {
+        return <div className='header-button'>
+            Men
+        </div>
+    }
+}
+
+export default HeaderButton
---
+++ src/templates/TemplateMain/index.jsx
@@ -0,0 +1,19 @@
+import './style.scss'
+
+import {Component} from 'react'
+
+class Main extends Component {
+    constructor (props) {
+        super(props)
+
+
+    }
+
+    render() {
+        return <div className='main'>
+            
+        </div>
+    }
+}
+
+export default Main
